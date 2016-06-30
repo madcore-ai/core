9511782931a096d74828d4cdc3a505942a6ac142
Peter Styk <peter@styk.tv>
2016-06-30 09:29:28 +0100

    parameters test

--- df_schedule_set.groovy
+++ df_schedule_set.groovy
@@ -1,4 +1,8 @@
 job('df.schedule.set') {
+    parameters {
+predefinedProp(String key, String value)
+choiceParam('myParameterName', ['option 1 (default)', 'option 2', 'option 3'], 'my description')
+    }
     triggers {
         cron("@hourly")
     }
