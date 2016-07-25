a1a3c47295d5bb2bf6f8a81fc2a745e753a4bc3c
Peter Styk <peter@styk.tv>
2016-07-25 22:20:33 +0100

    persmissions on installer

--- cb-init.sh
+++ cb-init.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash -v
 # Devops Factory Pre Configure
 # Maintained by Peter Styk (devopsfactory@styk.tv)
@@ -8,5 +9,6 @@ sudo apt-get install git -y
 sudo mkdir -p /opt/controlbox
 sudo chown ubuntu:ubuntu /opt/controlbox
 git clone https://bitbucket.org/ronaanimation/controlbox.git /opt/controlbox
+sudo chmod +x /opt/controlbox/cb-install.sh
 sudo "/opt/controlbox/cb-install.sh"
 
