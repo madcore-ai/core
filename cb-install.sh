356111e4a408aa557b5478fade68de46f1862fcf
Peter Styk <peter@styk.tv>
2016-07-29 11:48:59 +0100

    python-pip awscli added to install

--- cb-install.sh
+++ cb-install.sh
@@ -8,7 +8,7 @@ pushd /tmp
     sudo wget -q -O - https://jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
     sudo sh -c 'echo deb http://pkg.jenkins-ci.org/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
     sudo apt-get update
-    sudo apt-get install openjdk-8-jdk git jenkins -y
+    sudo apt-get install openjdk-8-jdk git jenkins python-pip awscli -y
     sudo groupadd hab && useradd -g hab -s /bin/bash -m hab
     sudo curl -fsSL https://get.docker.com/ | sh
     sudo usermod -aG docker jenkins
