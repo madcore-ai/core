b32ef77db1491bb8e1f806d9d85af036864ba131
Ramit Wadhwa <ramit_wadhwa@yahoo.co.in>
2016-08-05 20:01:52 +0530

    First Commit.
    Basic Script written. 
    Shell Script tested.
    groovy test pending.
    
    
    Signed-off-by: Ramit Wadhwa <ramit_wadhwa@yahoo.co.in>

---
+++ df_jenkins_user_create.groovy
@@ -0,0 +1,17 @@
+job('df.jenkins.user.create') {
+    parameters {
+        stringParam('username', '', '')
+        stringParam('password', '', '')
+    }
+    triggers {
+        cron("@minutes")
+    }
+    steps {
+        def command = """#!/bin/bash
+                        pushd /var/lib/jenkins/workspace/seed-dsl/controlbox
+                            bash df_jenkins_user_create.sh
+                        popd
+                        """
+        shell(command)
+    }
+}
