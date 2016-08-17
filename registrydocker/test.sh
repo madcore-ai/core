0346ec525f84df63db4d9d835d37097c8465cdce
indy singh <365.orion@gmail.com>
2016-08-17 14:39:07 +0000

    script for registery

---
+++ registrydocker/test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+cd /opt/controlbox/docker_registry_setup
+sudo docker run -d ubuntu /bin/sh -c "while true; do echo hello world; sleep 1; done"
+sudo docker run --rm --entrypoint htpasswd registry:2.5 -Bbn indy_user redhat  >> /opt/controlbox/auth/htpasswd
+sudo docker-compose stop
+sudo docker-compose up &
+sudo docker login -uindy_user -predhat localhost:5000/
+sudo docker tag $(docker images | grep ubuntu | awk '{print $3}') localhost:5000/ubuntu:image
+sudo docker push localhost:5000/ubuntu:image
+sudo docker stop $(docker ps -q --filter ancestor=ubuntu)
+sudo docker rmi -f $(docker images | grep "ubuntu" | awk '{print $3}')
+sudo docker pull localhost:5000/ubuntu:image
+sudo docker run -d localhost:5000/ubuntu:image /bin/sh -c "while true; do echo hello world; sleep 1; done"
