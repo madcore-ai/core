2718407ad759c4a73742129b40b72641a9579b9d
indy singh <365.orion@gmail.com>
2016-08-18 15:23:26 +0000

    New Setup.sh

--- registrydocker/setup.sh
+++ registrydocker/setup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 #su - jenkins
-#su - jenkins bash -c "cd /opt/controlbox/registrydocker; docker-compose up &"
+#su - jenkins bash -c "cd /opt/controlbox/registrydocker; docker-compose up &"i
+mkdir -p /opt/auth
+sudo docker run --rm --entrypoint htpasswd registry:2.5 -Bbn peter redhat  >> /opt/auth/htpasswd
+#sudo chmod +x /usr/local/bin/docker-compose
 cd /opt/controlbox/registrydocker
-docker-compose up &
+sudo docker-compose up &
 
