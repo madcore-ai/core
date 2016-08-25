be92be1715c2a2ea4021ed656cb1653ffc54d67c
Indy Singh <365.orion@gmail.com>
2016-08-25 08:10:36 +0000

    Permission

--- jenkins/df_docker_build_from_git.sh
+++ jenkins/df_docker_build_from_git.sh
@@ -3,12 +3,12 @@ REPO_PATH=/apache
 REPO_HTTPS=https://github.com/kstaken/dockerfile-examples.git
 WORKSPACE=/web_server
 DOCKER_NAME_LABEL=my-web-server
-git clone $REPO_HTTPS $WORKSPACE
-pushd $WORKSPACE/$REPO_PATH
-docker build -t=$DOCKER_NAME_LABEL .
-docker run -d -p 443:80 --name web my-web-server
-docker tag $(docker images | grep my-web-server | awk '{print $3}') localhost:5000/my-web-server:image
-docker push localhost:5000/my-web-server:image
+sudu git clone $REPO_HTTPS $WORKSPACE
+sudo pushd $WORKSPACE/$REPO_PATH
+sudo docker build -t=$DOCKER_NAME_LABEL .
+sudo docker run -d -p 443:80 --name web my-web-server
+sudo docker tag $(docker images | grep my-web-server | awk '{print $3}') localhost:5000/my-web-server:image
+sudo docker push localhost:5000/my-web-server:image
 
 popd
 
