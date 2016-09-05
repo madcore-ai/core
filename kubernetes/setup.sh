c27fd14fd5fd8cf11039c7a5a0a3b741accfdd43
Dan Jeshkov <d.jeshkov@gmail.com>
2016-09-05 16:49:21 +0100

    Systemd for kubernates

--- kubernetes/setup.sh
+++ kubernetes/setup.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 sudo chmod +x /usr/local/bin/docker-compose
 cd /opt/controlbox/kubernetes
-sudo docker-compose up &
+cp /opt/controlbox/kubernetes/docker-compose.service /etc/systemd/system/docker-compose-kubernetes.service
+# systemd reload
+systemctl daemon-reload
+# Enable the service
+cd /etc/systemd/system/
+systemctl enable docker-compose-kubernetes.service
+# Start the service
+systemctl start docker-compose-kubernetes
 
