66dcdcfb371be6cd6e416c98337b2e0dabf9c07d
root <root@xenial2>
2016-09-22 21:06:22 +0000

    changed network settings, added kubectl installation

--- heapster/setup.sh
+++ heapster/setup.sh
@@ -1,1 +1,3 @@
+wget -P /usr/local/bin/kubectl https://storage.googleapis.com/kubernetes-release/release/v1.3.4/bin/linux/amd64/kubectl
+chmod +x kubectl
 kubectl create -f /opt/controlbox/heapster/influxdb/
