05f1c5596795874e551f85749e1b041c2c118e26
Denis <hantden@gmail.com>
2016-09-27 11:07:41 +0000

    configured grafana+influxdb

--- heapster/setup.sh
+++ heapster/setup.sh
@@ -1,3 +1,3 @@
 wget -O /usr/local/bin/kubectl https://storage.googleapis.com/kubernetes-release/release/v1.3.4/bin/linux/amd64/kubectl
 chmod +x /usr/local/bin/kubectl
-#kubectl create -f /opt/controlbox/heapster/influxdb/
+kubectl create -f /opt/controlbox/heapster/influxdb/
