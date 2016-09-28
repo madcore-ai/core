c915107ff6314466fb8876c88897da98cdae5cf8
Denis <hantden@gmail.com>
2016-09-28 13:47:15 +0000

    delete cd command

--- kubernetes/setup.sh
+++ kubernetes/setup.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 ip=$(ip route get 8.8.8.8 | awk 'NR==1 {print $NF}')
 sudo chmod +x /usr/local/bin/docker-compose
-cd /opt/controlbox/kubernetes
 cp /opt/controlbox/kubernetes/docker-compose.service /etc/systemd/system/docker-compose-kubernetes.service
 mkdir -p /opt/kubernetes
 cat /opt/controlbox/kubernetes/docker-compose.yml.template | sed -e "s/\${ip}/${ip}/" > /opt/kubernetes/docker-compose.yml
