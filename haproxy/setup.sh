36d9bfff36be785debbec9390f8cb8e83026ebdb
root <root@ip-10-99-101-12.eu-west-1.compute.internal>
2016-10-03 13:01:53 +0000

    change haproxy setup script

--- haproxy/setup.sh
+++ haproxy/setup.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
+hostname=`wget -q -O - http://169.254.169.254/latest/meta-data/public-hostname`
 sudo apt-get install haproxy -y
 #certificate generated as part of main script cb-install.sh
+mkdir -p /opt/haproxy
+cat /opt/controlbox/haproxy/haproxy.cfg.template | sed -e "s/\hostname_tmpl/${hostname}/" > /opt/haproxy/haproxy.cfg
 sudo service haproxy stop
 sudo sh -c 'mv /etc/haproxy/haproxy.cfg /etc/haproxy/haproxy.cfg.old'
 sudo sh -c 'rm /etc/default/haproxy'
 sudo sh -c 'cp /opt/controlbox/haproxy/haproxy /etc/default/haproxy'
-sudo sh -c 'cp /opt/controlbox/haproxy/haproxy.cfg /etc/haproxy/haproxy.cfg'
+sudo sh -c 'cp /opt/controlbox/haproxy/haproxy.cfg.template /opt/haproxy/haproxy.cfg'
+sudo sh -c 'ln -s /etc/haproxy/haproxy.cfg /opt/haproxy/haproxy.cfg'
 sudo service haproxy start
