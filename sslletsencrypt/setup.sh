00313028f67f84cc559ada9b05875457c2ae09f4
root <root@ip-10-99-101-12.eu-west-1.compute.internal>
2016-10-04 20:45:52 +0000

    changed csr generation, added multiple domain

--- sslletsencrypt/setup.sh
+++ sslletsencrypt/setup.sh
@@ -1,49 +1,17 @@
 #!/bin/bash
-#hostname=`wget -q -O - http://169.254.169.254/latest/meta-data/public-hostname`
-hostname='controlbox.polfilm.devopshosted.com'
+#HOSTNAME=`wget -q -O - http://169.254.169.254/latest/meta-data/public-hostname`
+export SERVER='polfilm.devopshosted.com'
 email=devopsfactory@styk.tv
 
 sudo apt-get install letsencrypt -y
 
-mkdir -p /opt/sslletsencrypt
+/opt/controlbox/sslselfsigned/generate_CSR.sh
 
-echo "
-[ req ]
-default_bits = 2048 # Size of keys
-default_keyfile = key.pem # name of generated keys
-default_md = md5 # message digest algorithm
-string_mask = nombstr # permitted characters
-distinguished_name = req_distinguished_name
-
-[ req_distinguished_name ]
-# Variable name   Prompt string
-0.organizationName = Organization Name (company)
-organizationalUnitName = Organizational Unit Name (department, division)
-emailAddress = Email Address
-emailAddress_max = 40
-localityName = Locality Name (city, district)
-stateOrProvinceName = State or Province Name (full name)
-countryName = Country Name (2 letter code)
-countryName_min = 2
-countryName_max = 2
-commonName = Common Name (hostname, IP, or your name)
-commonName_max = 64
-
-countryName_default = GB
-localityName_default = London
-0.organizationName_default = Rona Animation Studios
-organizationalUnitName_default = Development
-commonName_default = $hostname
-
-[SAN]
-subjectAltName=DNS:$hostname"  > /opt/sslletsencrypt/openssl.cnf
-
-
-
-pushd /opt/sslletsencrypt
-    sudo openssl req -nodes -newkey rsa:2048 -keyout server.key -out server.der -outform der -config openssl.cnf -sha256 -batch -reqexts SAN
-    sudo letsencrypt certonly --csr /opt/sslletsencrypt/server.der --standalone --standalone-supported-challenges http-01 --email $email
+pushd /etc/pki/tls/certs
+    openssl req -inform pem -outform der -in server.csr -out server.der
+    sudo letsencrypt certonly --csr server.der --standalone --standalone-supported-challenges http-01 --email $email
     cat 0000_cert.pem server.key > /etc/pki/tls/certs/server.bundle.pem
+popd
 
-sudo service haproxy stop
-sudo service haproxy start
+#sudo service haproxy stop
+#sudo service haproxy start
--- sslselfsigned/setup.sh
+++ sslselfsigned/setup.sh
@@ -1,37 +1,9 @@
 mkdir -p /etc/pki/tls/certs
 export SERVER=$HOSTNAME
-echo "
-[ req ]
-default_bits = 2048 # Size of keys
-default_keyfile = key.pem # name of generated keys
-default_md = md5 # message digest algorithm
-string_mask = nombstr # permitted characters
-distinguished_name = req_distinguished_name
 
-[ req_distinguished_name ]
-# Variable name   Prompt string
-0.organizationName = Organization Name (company)
-organizationalUnitName = Organizational Unit Name (department, division)
-emailAddress = Email Address
-emailAddress_max = 40
-localityName = Locality Name (city, district)
-stateOrProvinceName = State or Province Name (full name)
-countryName = Country Name (2 letter code)
-countryName_min = 2
-countryName_max = 2
-commonName = Common Name (hostname, IP, or your name)
-commonName_max = 64
-
-countryName_default = GB
-localityName_default = London
-0.organizationName_default = Rona Animation Studios
-organizationalUnitName_default = Development
-commonName_default = $SERVER" > /etc/pki/tls/certs/openssl.cnf
+/opt/controlbox/sslselfsigned/generate_CSR.sh
 
 pushd /etc/pki/tls/certs
-    sudo openssl genrsa -des3 -passout pass:TasWq -out serverPAS.key 2048
-    sudo openssl req -new -key serverPAS.key -passin pass:TasWq -out server.csr -config openssl.cnf -batch
-    sudo openssl x509 -req -days 365 -in server.csr -signkey serverPAS.key -passin pass:TasWq -out server.crt
-    sudo openssl rsa -passin pass:TasWq -in serverPAS.key -out server.key
+    sudo openssl x509 -req -days 365 -in server.csr -signkey server.key -out server.crt
     cat server.crt server.key > server.bundle.pem
 popd
