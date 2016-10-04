7071d83b89839cd6bf1c5e7b939f20f8a2e239f2
root <root@xenial2>
2016-10-04 11:26:14 +0000

    added information messages

--- heapster/setup.sh
+++ heapster/setup.sh
@@ -5,9 +5,13 @@ wget -O /opt/bin/kubectl https://storage.googleapis.com/kubernetes-release/relea
 chmod +x /opt/bin/kubectl
 ln -s /opt/bin/kubectl /usr/local/bin/kubectl
 fi
+
+echo “Waiting for kubernetes-dashboard (may take few minutes) …”
 sudo su -c "until curl -sL -w '%{http_code}' 'http://localhost:8080/api/v1/proxy/namespaces/kube-system/services/kubernetes-dashboard' -o /dev/null | grep -m 1 '200'; do : ; done" jenkins
+echo “kubernetes-dashboard confirmed.”
 
 ip=$(ip route get 8.8.8.8 | awk 'NR==1 {print $NF}')
+
 mkdir -p /opt/heapster
 pushd /opt/controlbox/heapster/
    cat heapster-controller.yaml | sed -e "s/\${ip}/${ip}/" > /opt/heapster/heapster-controller.yaml
@@ -16,4 +20,9 @@ pushd /opt/controlbox/heapster/
    cp influxdb-service.yaml /opt/heapster/influxdb-service.yaml
    cp heapster-service.yaml /opt/heapster/heapster-service.yaml
 popd
+
 kubectl create -f /opt/heapster
+
+echo “Waiting for monitoring-grafana (may take few minutes) …”
+sudo su -c "until curl -sL -w '%{http_code}' 'http://localhost:8080/api/v1/proxy/namespaces/kube-system/services/monitoring-grafana' -o /dev/null | grep -m 1 '200'; do : ; done" jenkins
+echo “monitoring-grafana confirmed”
