e9aeaecbc6e554e1eeaff4796cc214022a88a4bf
root <hantden@gmail.com>
2016-10-07 19:55:05 +0000

    added source to grafana

--- jenkins_metrics/setup.sh
+++ jenkins_metrics/setup.sh
@@ -12,5 +12,6 @@ rm -rf /var/lib/jenkins/jenkinsci.plugins.influxdb.InfluxDbPublisher.xml
 cp /opt/controlbox/jenkins_metrics/jenkinsci.plugins.influxdb.InfluxDbPublisher.xml /var/lib/jenkins/jenkinsci.plugins.influxdb.InfluxDbPublisher.xml 
 curl -i -GET http://localhost:8086/query --data-urlencode "q=CREATE DATABASE jenkins_logs"
 curl -i -GET http://localhost:8086/query --data-urlencode "q=CREATE USER test WITH PASSWORD '12345'"
+curl --user admin:admin -X POST -H 'Content-Type: application/json;charset=UTF-8' --data-binary "{\"name\":\"jenkins\",\"type\":\"influxdb\",\"url\":\"http://127.0.0.1:8086\",\"access\":\"proxy\",\"database\":\"jenkins_logs\",\"user\":\"jenkins\",\"password\":\"12345\"}" "http://127.0.0.1:3000/api/datasources"
 service jenkins restart
 
