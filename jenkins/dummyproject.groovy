15c5f8af85dc0bbe11452d8b172dbbec52941172
Denis <hantden@gmail.com>
2016-10-10 21:38:51 +0000

    added groovy for test job

--- jenkins/dummyproject.groovy
+++ jenkins/dummyproject.groovy
@@ -8,15 +8,11 @@ popd
         shell(command)
     }
 
-#    publishers {
-#	influxdb(http://127.0.0.1:8086,jenkins_logs)
-#    }
+    configure { project ->
 
-#  <publishers>
-#    <jenkinsci.plugins.influxdb.InfluxDbPublisher plugin="influxdb@1.8.1">
-#      <selectedTarget>http://127.0.0.1:8086,jenkins_logs</selectedTarget>
-#      <customPrefix>test</customPrefix>
-#    </jenkinsci.plugins.influxdb.InfluxDbPublisher>
-# </publishers>
+    project / 'publishers' << 'jenkinsci.plugins.influxdb.InfluxDbPublisher plugin="influxdb@1.8.1"' {
+      selectedTarget 'http://127.0.0.1:8086,jenkins_logs'
+	}
+    }
 
 }
