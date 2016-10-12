6f61fed2e710c65ef650d58b664c5220e151050d
root <root@ip-10-99-101-12.eu-west-1.compute.internal>
2016-10-12 14:21:02 +0000

    fixed template

--- jenkins/df_ssl_letsencrypt_getandinstall.sh
+++ jenkins/df_ssl_letsencrypt_getandinstall.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 echo "EMAIL: '$EMAIL'"
+EMAIL=devopsfactory@styk.tv
 pushd /opt/certs
     openssl req -inform pem -outform der -in server.csr -out server.der
-    letsencrypt certonly --csr server.der --standalone --standalone-supported-challenges http-01 --email $EMAIL
+    letsencrypt certonly --csr server.der --standalone --email $EMAIL --standalone-supported-challenges http-01
     cat 0000_cert.pem server.key > server.bundle.pem
 popd
 sed -i "s/\/etc\/pki\/tls\/certs\/server.bundle.pem/\/opt\/certs\/server.bundle.pem/g" /opt/haproxy/haproxy.cfg
