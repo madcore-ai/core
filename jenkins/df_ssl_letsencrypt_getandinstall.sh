223d9690dcf787635deefef35c333a9b791386c7
root <root@ip-10-99-101-12.eu-west-1.compute.internal>
2016-10-12 13:37:28 +0000

    chenged jenkins job

--- jenkins/df_ssl_letsencrypt_getandinstall.sh
+++ jenkins/df_ssl_letsencrypt_getandinstall.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 echo "EMAIL: '$EMAIL'"
 pushd /opt/certs
-echo $EMAIL
     openssl req -inform pem -outform der -in server.csr -out server.der
     letsencrypt certonly --csr server.der --standalone --standalone-supported-challenges http-01 --email $EMAIL
     cat 0000_cert.pem server.key > server.bundle.pem
 popd
 sed -i "s/\/etc\/pki\/tls\/certs\/server.bundle.pem/\/opt\/certs\/server.bundle.pem/g" /opt/haproxy/haproxy.cfg
-systemctl restart haproxy
+service haproxy stop
+service haproxy start
