b5d3c699abb9c7b2f690d0a8c7490fb4b52d827a
root <root@ip-10-99-101-213.eu-west-1.compute.internal>
2016-10-18 10:19:35 +0000

    changed groovy for test job

--- jenkins/df_deploy_kubernetes_test.sh
+++ jenkins/df_deploy_kubernetes_test.sh
@@ -4,7 +4,7 @@ git clone $REPO1_HTTPS /var/lib/jenkins/workspace/df.deploy.kubernetes.test/flas
 pushd /var/lib/jenkins/workspace/df.deploy.kubernetes.test/flask-test
     docker build -t flask-test .
     docker tag flask-test localhost:5000/flask-test
-    docker login -upeter -predhat localhost:5000/
+    docker login -upeter -predhat localhost:5000
     docker push  localhost:5000/flask-test
     kubectl create secret docker-registry myregistrykey --docker-server=localhost:5000 --docker-username=peter --docker-password=redhat --docker-email=test@test.com
     kubectl create -f /var/lib/jenkins/workspace/df.deploy.kubernetes.test/flask-test/kubernetes
