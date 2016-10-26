8baadaa65f055d4b3fff2c081260df8d8afb13aa
root <root@ip-10-99-101-4.eu-west-1.compute.internal>
2016-10-26 16:25:13 +0000

    fixed issues

--- jenkins/df_registration.groovy
+++ jenkins/df_registration.groovy
@@ -19,7 +19,7 @@ pipelineJob('df.registration') {
                     stage 'Update owner info in redis'
 		    build job: 'df.redis.owner.update', parameters: [string(name: 'Hostname', value: params.Hostname), string(name: 'Email', value: params.Email), string(name: 'OrganizationName', value: params.OrganizationName), string(name: 'OrganizationalUnitName', value: params.OrganizationalUnitName), string(name: 'LocalityName', value: params.LocalityName), string(name: 'Country', value: params.Country)]
                     stage 'Update app info in redis'
-		    build job: 'df.redis.app.update', string(name: 'APPNAME', value: params.APPNAME)
+		    build job: 'df.redis.app.update', parameters: [string(name: 'APPNAME', value: params.APPNAME)]
 		    stage 'generate csr'
 		    build 'df.ssl.csr.generate'
 		    stage 'get certificate and reconfigure haproxy'
