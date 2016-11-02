b9dca856f352b3d485ea0a3dbcd3d1f35cd1ba91
root <root@xenial2>
2016-11-02 22:05:54 +0000

    reconfigures registry and haproxy

--- registrydocker/setup.sh
+++ registrydocker/setup.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
-#su - jenkins
-#su - jenkins bash -c "cd /opt/controlbox/registrydocker; docker-compose up &"i
 sudo mkdir -p /opt/auth
-sudo docker run --rm --entrypoint htpasswd registry:2.5 -Bbn peter redhat  >> /opt/auth/htpasswd
-size=$(wc -l /opt/auth/htpasswd | grep -o '[0-9]*')
-if (($size < 2));
-then
-sudo htpasswd -b /opt/auth/htpasswd peter redhat
-fi
+#sudo docker run --rm --entrypoint htpasswd registry:2.5 -Bbn peter redhat  >> /opt/auth/htpasswd
+#size=$(wc -l /opt/auth/htpasswd | grep -o '[0-9]*')
+#if (($size < 2));
+#then
+#sudo htpasswd -b /opt/auth/htpasswd controlbox registrypass
+#fi
 sudo chmod +x /usr/local/bin/docker-compose
 sudo cp /opt/controlbox/registrydocker/docker-compose.service /etc/systemd/system/docker-compose.service
 # systemd reload
