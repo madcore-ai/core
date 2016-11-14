0d38ee14cd8edb018ca72de4983a1efdff69980b
root <root@xenial2>
2016-11-14 17:51:28 +0000

    fix path to script

--- jenkins/df_selftest_docker_registry.groovy
+++ jenkins/df_selftest_docker_registry.groovy
@@ -3,7 +3,7 @@ job('df.selftest.docker.registry') {
     steps {
         def command = """#!/bin/bash
 	pushd /var/lib/jenkins/workspace/seed-dsl/controlbox/jenkins
-	    bash df_selftest_docker_regidtry.sh
+	    bash df_selftest_docker_registry.sh
 	popd
 
 """
