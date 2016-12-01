327d676290b86a635ea578dd7f1f8eb38b43398b
Chirica Gheorghe <chiricagheorghe@gmail.com>
2016-12-01 09:54:27 +0200

    Add spak jar examples
    Former-commit-id: ad02cdfecc87f149c70b5da9d0ee571abf4a4842

--- jenkins/madcore_spark_submit.groovy
+++ jenkins/madcore_spark_submit.groovy
@@ -1,9 +1,9 @@
-job('df.deploy.spark') {
+job('madcore.spark.submit') {
     wrappers { preBuildCleanup() }
     parameters {
-        stringParam('APP_FILE_NAME', '', '')
-	    stringParam('APP_ARGS', '', '')
-	    stringParam('SPARK_ARGS', '', '')
+        stringParam('APP_FILE_NAME', 'spark-examples-1.5.2-hadoop2.6.0.jar', '')
+	    stringParam('APP_ARGS', '10', '')
+	    stringParam('SPARK_ARGS', '--class org.apache.spark.examples.SparkPi --executor-memory 1G', '')
     }
     steps {
         def command = """#!/bin/bash
