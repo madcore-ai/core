e3c692697aa585aa98234214c9ed912724f80da4
Chirica Gheorghe <chiricagheorghe@gmail.com>
2016-12-07 14:32:36 +0200

    Add cfn-init script

---
+++ kubernetes/cluster/cfn-init.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -v
+
+sudo apt-get update
+sudo apt-get install -y python-pip
+sudo pip install -U pip
+sudo pip install https://s3.amazonaws.com/cloudformation-examples/aws-cfn-bootstrap-latest.tar.gz
+
+# TODO make this configurable
+sudo /usr/local/bin/cfn-init \
+               --stack Madcore-Cluster \
+               --resource MadcoreClusterAutoScalingLaunchConfiguration \
+               --configsets ClusterConfig \
+               --region eu-west-1
