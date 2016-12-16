885f778e52aadf4257915e80c2266e57595d8dce
Chirica Gheorghe <chiricagheorghe@gmail.com>
2016-12-16 17:44:43 +0200

    Added volumne to pod

--- bin/deploy_container.py
+++ bin/deploy_container.py
@@ -2,7 +2,7 @@ from __future__ import print_function
 import sys
 from utils import docker_build_from_repo, kubectl_create_secret, run_cmd, j2_render, wait_until_pod_is_ready, get_pod, \
     execute_cmd_on_pod
-from consts import registry_secret
+from consts import registry_secret, docker_server
 
 job_name = 'madcore.deploy.container'
 repo_url = sys.argv[1]
@@ -14,7 +14,9 @@ workspace = '/var/lib/jenkins/workspace/' + job_name + "/" + app_name + "/"
 repo_dest = workspace + 'repo/'
 kub_config_path = workspace + 'kube_config/'
 app_port = 1234
-namespace = 'madcore_plugins'
+namespace = 'madcore-plugins'
+volume_mount_path = '/opt/s3'
+volume_host_path = '/opt/s3'
 
 run_cmd("mkdir -p %s" % kub_config_path)
 
@@ -22,6 +24,7 @@ docker_build_from_repo(
     repo_url=repo_url,
     app_name=app_name,
     repo_dest=repo_dest,
+    branch_name='development',
     repo_folder=app_name
 )
 
@@ -32,13 +35,17 @@ data_ns = {
 }
 j2_render('/opt/madcore/bin/templates/namespace.yaml', kub_config_path + 'ns.yaml', data_ns)
 
+
 data_rc = {
     'name': app_name,
     'image': app_name + ':image',
     'registry_secret': registry_secret,
+    'docker_server': docker_server,
     'namespace': namespace,
+    'volume_mount_path': volume_mount_path,
+    'volume_host_path': volume_host_path
 }
-j2_render('/opt/madcore/bin/templates/kub_replication_controller_template.yaml',
+j2_render('/opt/madcore/bin/templates/kub_replication_controller_template_with_volume.yaml',
           kub_config_path + 'rc.yaml', data_rc)
 
 data_srv = {
--- jenkins/madcore_plugins_slack_pull.groovy
+++ jenkins/madcore_plugins_slack_pull.groovy
@@ -1,7 +1,7 @@
 job('madcore.plugin.slack.pull') {
     wrappers { preBuildCleanup() }
     parameters {
-	    stringParam('REPO_URL', 'git@github.com:madcore-ai/containers.git', '')
+	    stringParam('REPO_URL', 'https://github.com/madcore-ai/containers.git', '')
         stringParam('APP_NAME', 'slack', '')
         stringParam('SLACK_TOKEN', 'xoxp-102271888193-108964168741-116823616578-24874f6421c8051ac92247b017277533', '')
         stringParam('SLACK_PAYLOAD', '{"channel": "tests"}', '')
@@ -15,7 +15,7 @@ echo "SLACK_TOKEN: '\$SLACK_TOKEN'"
 echo "SLACK_PAYLOAD: '\$SLACK_PAYLOAD'"
 echo "S3_BUCKET: '\$S3_BUCKET'"
 
-    python /opt/madcore/bin/deploy_container.py "\$REPO_URL" "\$APP_NAME" "madcore_slack -t \$SLACK_TOKEN -a pull_messages -b \$S3_BUCKET -p \$SLACK_PAYLOAD"
+    python /opt/madcore/bin/deploy_container.py "\$REPO_URL" "\$APP_NAME" "madcore_slack -t \$SLACK_TOKEN -a pull_messages -b \$S3_BUCKET -p '\$SLACK_PAYLOAD'"
 """
         shell(command)
     }
