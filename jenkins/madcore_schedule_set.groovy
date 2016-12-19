92e3614924f9f225c545b01b8e2a2285b3f7f901
Chirica Gheorghe <chiricagheorghe@gmail.com>
2016-12-19 21:52:12 +0200

    Fixed input params for schedule set job

--- jenkins/madcore_schedule_set.groovy
+++ jenkins/madcore_schedule_set.groovy
@@ -1,14 +1,20 @@
 job('madcore.schedule.set') {
     parameters {
-		stringParam('SchedulerData', '{"name": "test", "region": "eu-west-1", "instances_list": ["i-39dc50dd"], "s_mo": "100000000001111110000000", "s_tu": "000000000001111110000000", "s_we": "000000000000000000001111", "s_th": "111111000000000000000000", "s_fr": "000000000000000000000000", "s_sa": "000000000000000000000000", "s_su": "000000000000000000000001"}', '')
-    }
-    triggers {
-        cron("@hourly")
+        stringParam('Name', 'test', '')
+        stringParam('Region', 'eu-west-1', '')
+        stringParam('Monday', '100000000001111110000000', '')
+        stringParam('Tuesday', '000000000001111110000000', '')
+        stringParam('Wednesday', '000000000000000000001111', '')
+        stringParam('Thursday', '111111000000000000000000', '')
+        stringParam('Friday', '000000000000000000000000', '')
+        stringParam('Saturday', '000000000000000000000000', '')
+        stringParam('Sunday', '000000000000000000000001', '')
+        stringParam('InstanceList', 'i-39dc50dd', '')
+        booleanParam('IsEnabled', true, '')
     }
     steps {
         def command = """#!/bin/bash
-echo "SchedulerData: '\$SchedulerData'"
-python /opt/madcore/bin/schgen.py --json "\$SchedulerData" -d
+python /opt/madcore/bin/schgen.py --json "{\\"enabled\\":\$IsEnabled, \\"name\\":\\"\$Name\\", \\"region\\":\\"\$Region\\", \\"instances_list\\":\\"\$InstanceList\\", \\"s_mo\\":\\"\$Monday\\", \\"s_tu\\": \\"\$Tuesday\\", \\"s_we\\": \\"\$Wednesday\\", \\"s_th\\": \\"\$Thursday\\", \\"s_fr\\": \\"\$Friday\\",\\"s_sa\\": \\"\$Saturday\\",\\"s_su\\": \\"\$Sunday\\"}" -d
 """
         shell(command)
     }
