2e17b859ef9638a5748842a7db9c0f1a1c4d5758
Denis Sokolov <hantden@gmail.com>
2016-12-21 14:04:33 +0200

    added registry login

---
+++ jenkins/madcore_docker_registry_delete.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+echo "IMAGE_NAME: '$IMAGE_NAME'"
--- jenkins/madcore_docker_registry_status.groovy
+++ jenkins/madcore_docker_registry_status.groovy
@@ -1,6 +1,7 @@
 job('madcore.docker.registry.status') {
     steps {
         def command = """#!/bin/bash
+docker login -uroot -pmadcore localhost:5000/
 echo $(curl http://myregistry:5000/v1/search?)
 """
         shell(command)
