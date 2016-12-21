26fb22320fd0f25f5db37c016603f5e47849da2b
root <root@xenial2>
2016-12-21 12:30:09 +0000

    fixed docker jobs

--- jenkins/madcore_docker_registry_delete.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-echo "IMAGE_NAME: '$IMAGE_NAME'"
--- jenkins/madcore_docker_registry_status.groovy
+++ jenkins/madcore_docker_registry_status.groovy
@@ -1,8 +1,7 @@
 job('madcore.docker.registry.status') {
     steps {
         def command = """#!/bin/bash
-docker login -uroot -pmadcore localhost:5000/
-echo $(curl http://myregistry:5000/v1/search?)
+echo \$(curl -k https://root:madcore@localhost:5000/v2/_catalog)
 """
         shell(command)
     }
