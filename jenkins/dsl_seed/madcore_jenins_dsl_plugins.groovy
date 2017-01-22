2de17cb5cc16fe036d565378d7159df62b2cb4d1
Chirica Gheorghe <chiricagheorghe@gmail.com>
2017-01-22 21:33:03 +0200

    Removed params for plugin dsl job

--- jenkins/dsl_seed/madcore_jenins_dsl_plugins.groovy
+++ jenkins/dsl_seed/madcore_jenins_dsl_plugins.groovy
@@ -1,15 +1,12 @@
 job('madcore.jenkins.dsl.seed.plugins') {
     customWorkspace('/opt/plugins')
 
-    parameters {
-        stringParam('BRANCH', 'master', '')
-    }
     scm {
         git {
             remote {
                 url('https://github.com/madcore-ai/plugins')
             }
-            branch(params.BRANCH)
+            branch('master')
         }
     }
 
