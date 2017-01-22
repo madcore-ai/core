5bf99c61fdaf1bf92a34a722fc8974b169dcfcd6
Chirica Gheorghe <chiricagheorghe@gmail.com>
2017-01-22 20:54:48 +0200

    Added wait for service down job

--- kubernetes/kubectl/wait_for_service_down.sh
+++ kubernetes/kubectl/wait_for_service_down.sh
@@ -3,9 +3,9 @@
 APP_NAME="$1"
 SERVICE_NAME="$2"
 SERVICE_NAMESPACE="$3"
-HTTP_OK_CODE=200
+HTTP_OK_CODE=404
 
 # TODO@geo Maybe we can use standard kubectl to make this check?
-echo "Waiting for ${APP_NAME} ${SERVICE_NAME} to start (may take few minutes) …"
+echo "Waiting for ${APP_NAME} ${SERVICE_NAME} to stop (may take few minutes) …"
 until curl -sL -w '%{http_code}' 'http://localhost:8080/api/v1/proxy/namespaces/${SERVICE_NAMESPACE}/services/${SERVICE_NAME}/' -o /dev/null | grep -m 1 "${HTTP_OK_CODE}"; do : ; done
-echo "${APP_NAME}  started."
+echo "${APP_NAME}  stopped."
