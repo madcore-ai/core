9b201331ce45024e592e6ca92a9fb8a2b20d3da9
Chirica Gheorghe <chiricagheorghe@gmail.com>
2017-01-22 20:44:23 +0200

    Fixed wait until kub service up

--- kubernetes/kubectl/wait_for_service_up.sh
+++ kubernetes/kubectl/wait_for_service_up.sh
@@ -4,6 +4,6 @@ APP_NAME="$1"
 SERVICE_NAME="$2"
 SERVICE_NAMESPACE="$3"
 # TODO@geo Maybe we can use standard kubectl to make this check?
-echo "Waiting for ${APP_NAME} (may take few minutes) …"
-sudo su -c "until curl -sL -w '%{http_code}' 'http://localhost:8080/api/v1/proxy/namespaces/${SERVICE_NAMESPACE}/services/${SERVICE_NAME}/' -o /dev/null | grep -m 1 '200'; do : ; done" jenkins
+echo "Waiting for ${APP_NAME} ${SERVICE_NAME} (may take few minutes) …"
+until curl -sL -w '%{http_code}' 'http://localhost:8080/api/v1/proxy/namespaces/${SERVICE_NAMESPACE}/services/${SERVICE_NAME}/' -o /dev/null | grep -m 1 '200'; do : ; done
 echo "${APP_NAME}  confirmed."
