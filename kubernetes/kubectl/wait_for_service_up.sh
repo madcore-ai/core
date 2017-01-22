f3bf9129a7a7304bd8864817a45895c9db99f364
Chirica Gheorghe <chiricagheorghe@gmail.com>
2017-01-22 21:13:31 +0200

    Fixed bash single quote strings

--- kubernetes/kubectl/wait_for_service_up.sh
+++ kubernetes/kubectl/wait_for_service_up.sh
@@ -7,5 +7,5 @@ HTTP_OK_CODE=200
 
 # TODO@geo Maybe we can use standard kubectl to make this check?
 echo "Waiting for ${APP_NAME} ${SERVICE_NAME} to start (may take few minutes) …"
-until curl -sL -w '%{http_code}' 'http://localhost:8080/api/v1/proxy/namespaces/${SERVICE_NAMESPACE}/services/${SERVICE_NAME}/' -o /dev/null | grep -m 1 "${HTTP_OK_CODE}"; do : ; done
+until curl -sL -w '%{http_code}' "http://localhost:8080/api/v1/proxy/namespaces/${SERVICE_NAMESPACE}/services/${SERVICE_NAME}/" -o /dev/null | grep -m 1 "${HTTP_OK_CODE}"; do : ; done
 echo "${APP_NAME}  started."
