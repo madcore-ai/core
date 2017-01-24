927414b62d89bab4290796e36245f572947d0e1c
Chirica Gheorghe <chiricagheorghe@gmail.com>
2017-01-24 21:54:04 +0200

    Add cluster name label to kub nodes

--- kubernetes/cluster/setup.sh
+++ kubernetes/cluster/setup.sh
@@ -12,6 +12,7 @@ pushd /opt/madcore/kubernetes/cluster/
     ln -s /opt/docker-compose/docker-compose-kubernetes.service /etc/systemd/system/docker-compose-kubernetes.service
     mkdir -p /opt/kubernetes
     cat docker-compose.yml.template | sed -e "s/\${ip}/${KUB_MASTER_IP}/" > /opt/kubernetes/docker-compose.yml
+    cat docker-compose.yml.template | sed -e "s/\${cluster_name}/${KUB_CLUSTER_NAME}/" > /opt/kubernetes/docker-compose.yml
 popd
 
 # systemd reload
