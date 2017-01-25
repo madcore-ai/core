87a4268e2006ca2f868c3c1dd8709ac2c04f942b
Chirica Gheorghe <chiricagheorghe@gmail.com>
2017-01-25 05:38:17 +0200

    Fixed unused mv

--- kubernetes/cluster/setup.sh
+++ kubernetes/cluster/setup.sh
@@ -12,7 +12,6 @@ pushd /opt/madcore/kubernetes/cluster/
     ln -s /opt/docker-compose/docker-compose-kubernetes.service /etc/systemd/system/docker-compose-kubernetes.service
     mkdir -p /opt/kubernetes
     cat docker-compose.yml.template | sed -e "s/\${ip}/${KUB_MASTER_IP}/" | sed -e "s/\${cluster_name}/${KUB_CLUSTER_NAME}/" > /opt/kubernetes/docker-compose.yml
-    cp docker-compose.yml.template /opt/kubernetes/docker-compose.yml
 popd
 
 # systemd reload
