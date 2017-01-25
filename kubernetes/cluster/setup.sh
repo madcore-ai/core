9cbb33307cbc31e59c9d230673fea6d2c92cdfa0
Chirica Gheorghe <chiricagheorghe@gmail.com>
2017-01-25 05:37:37 +0200

    Fixed sed replace

--- kubernetes/cluster/setup.sh
+++ kubernetes/cluster/setup.sh
@@ -11,8 +11,7 @@ pushd /opt/madcore/kubernetes/cluster/
     cp docker-compose.service /opt/docker-compose/docker-compose-kubernetes.service
     ln -s /opt/docker-compose/docker-compose-kubernetes.service /etc/systemd/system/docker-compose-kubernetes.service
     mkdir -p /opt/kubernetes
-    cat docker-compose.yml.template | sed -e "s/\${ip}/${KUB_MASTER_IP}/" > docker-compose.yml.template
-    cat docker-compose.yml.template | sed -e "s/\${cluster_name}/${KUB_CLUSTER_NAME}/" > docker-compose.yml.template
+    cat docker-compose.yml.template | sed -e "s/\${ip}/${KUB_MASTER_IP}/" | sed -e "s/\${cluster_name}/${KUB_CLUSTER_NAME}/" > /opt/kubernetes/docker-compose.yml
     cp docker-compose.yml.template /opt/kubernetes/docker-compose.yml
 popd
 
