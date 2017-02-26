87e4658e5144fecbf6a115df4a377180e98d1da3
Denys <hantden@gmail.com>
2017-02-26 23:02:00 +0200

    add template and script for ingress

---
+++ bin/render_ingress.py
@@ -0,0 +1,8 @@
+import sys, os, json, jinja2
+from jinja2 import Template
+
+config_template = open('/opt/madcore/bin/templates/ingress.template').read()
+template = Template(config_template)
+config = (template.render(HOST=sys.argv[1], SERVICE_NAME=sys.argv[2], SERVICE_PORT=sys.argv[3])
+os.system("mkdir -p /opt/ingress/")
+open("/opt/ingress/" + sys.argv[2] + ".yaml", "w").write(config)
