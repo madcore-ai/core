69289f700d13cdaef13527b2e67e87a21c709555
Denys <hantden@gmail.com>
2017-02-27 14:11:53 +0200

    add script for render configmp

---
+++ bin/render_ingress_configmap.py
@@ -0,0 +1,13 @@
+import sys, os, json, jinja2, redis
+from jinja2 import Template
+
+i_key = "owner-info"
+r_server = redis.StrictRedis('127.0.0.1', db=2)
+json_data = r_server.get(i_key)
+if json_data is not None:
+    email = data['Email']
+
+config_template = open('/opt/plugins/cluster/ingress/kub/lego_configmap.template').read()
+template = Template(config_template)
+config = (template.render(email=email))
+open("/opt/plugins/cluster/ingress/kub/lego_configmap.yaml", "w").write(config)
