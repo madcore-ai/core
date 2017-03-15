2696ab755bafe4b6ab728fa4c5ac03600734b964
Denys <hantden@gmail.com>
2017-03-15 10:28:14 +0200

    change render ingress script

--- bin/render_ingress.py
+++ bin/render_ingress.py
@@ -1,7 +1,16 @@
-import sys, os, json, jinja2
+import sys, os, json, jinja2, redis
 from jinja2 import Template
+
+r_server = redis.StrictRedis('127.0.0.1', db=2)
+i_key = "owner-info"
+json_data = r_server.get(i_key)
+if json_data is not None:
+    data = json.loads(json_data)
+    main_domain = data['Hostname']
+
+fqdn = sys.argv[1] + "ext." + main_domain
 
 config_template = open('/opt/madcore/bin/templates/ingress.template').read()
 template = Template(config_template)
-config = (template.render(HOST=sys.argv[1], SERVICE_NAME=sys.argv[2], SERVICE_PORT=sys.argv[3], NAMESPACE=sys.argv[4]))
+config = (template.render(HOST=fqdn, SERVICE_NAME=sys.argv[2], SERVICE_PORT=sys.argv[3], NAMESPACE=sys.argv[4]))
 open("/opt/ingress/" + sys.argv[2] + ".yaml", "w").write(config)
