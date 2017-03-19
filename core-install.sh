0af52462472ab88ee2d32fff3cda23f5c94c223b
Denys <hantden@gmail.com>
2017-03-19 19:09:05 +0200

    fix jenkins repo

--- core-install.sh
+++ core-install.sh
@@ -10,8 +10,8 @@ echo "INSTALLING CORE OF MADCORE"
 
 # PREREQUESITES
 pushd /tmp
-    sudo wget -q -O - https://jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
-    sudo sh -c 'echo deb http://pkg.jenkins-ci.org/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+    sudo wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
+    sudo sh -c 'echo deb https://pkg.jenkins.io/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
     sudo apt-get update
     sudo apt-get install openjdk-8-jdk git jenkins python-pip awscli haproxy letsencrypt libcurl4-gnutls-dev librtmp-dev apache2-utils jq -y
     sudo pip install --upgrade pip
