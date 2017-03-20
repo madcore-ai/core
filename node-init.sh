9305282218d118369b8ccd5276b868d736e732ba
Denys <hantden@gmail.com>
2017-03-20 12:58:25 +0200

    move disable sd code

--- node-init.sh
+++ node-init.sh
@@ -6,9 +6,7 @@ sudo echo ENV=AWS >> /etc/environment
 
 KUB_MASTER_IP="${KUB_MASTER_IP:-10.99.101.99}"
 
-# disable source-destination health check
-INSTANCE_ID=$(curl http://169.254.169.254/latest/meta-data/instance-id)
-aws ec2 modify-instance-attribute --instance-id ${INSTANCE_ID} --no-source-dest-check --region ${AWS_REGION}
+
 
 # PRECONFIGURE madcore
 sudo apt-get update
@@ -16,6 +14,7 @@ sudo apt-get install git -y
 sudo mkdir -p /opt/madcore
 sudo chown ubuntu:ubuntu /opt/madcore
 git clone https://github.com/madcore-ai/core.git /opt/madcore
+
 
 sudo chmod +x /opt/madcore/kubernetes/cluster/node-install.sh
 sudo "/opt/madcore/kubernetes/cluster/node-install.sh"
