3ccaa265b51f33df48473e3eb35b5dde3c45f637
Denys Sokolov <hantden@gmail.com>
2017-03-24 20:32:37 +0200

    fix syntax

--- jenkins/kubectl/madcore_kubectl_wait_for_pod_up.groovy
+++ jenkins/kubectl/madcore_kubectl_wait_for_pod_up.groovy
@@ -6,7 +6,7 @@ job('madcore.kubectl.wait.pod.up') {
     steps {
         def command = """#!/bin/bash
 until [[ $status == "Running" ]]; do
-status=$(kubectl get pods --all-namespaces | grep \$POD_NAME | awk '{print \$4}')
+status=\$(kubectl get pods --all-namespaces | grep \$POD_NAME | awk '{print \$4}')
 done
 """
         shell(command)
