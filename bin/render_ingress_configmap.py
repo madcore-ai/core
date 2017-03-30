27daf55219c80b8c5847d300f5346401f1a73ae9
Peter Styk <peter@styk.tv>
2017-03-30 10:40:59 +0100

    plugins root path update

--- bin/render_ingress_configmap.py
+++ bin/render_ingress_configmap.py
@@ -8,7 +8,7 @@ if json_data is not None:
     data = json.loads(json_data)
     email = data['Email']
 
-config_template = open('/opt/plugins/cluster/ingress/kub/lego_configmap.template').read()
+config_template = open('/opt/plugins/ingress/kub/lego_configmap.template').read()
 template = Template(config_template)
 config = (template.render(email=email))
-open("/opt/plugins/cluster/ingress/kub/lego_configmap.yaml", "w").write(config)
+open("/opt/plugins/ingress/kub/lego_configmap.yaml", "w").write(config)
