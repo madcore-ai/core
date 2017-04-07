ff9180ee2717e46677db52c752313567b4bdd00d
Peter Styk <peter@styk.tv>
2017-04-07 12:02:52 +0100

    reverting back to no check

--- jenkins/ssl/madcore_ssl_letsencrypt_getandinstall.groovy
+++ jenkins/ssl/madcore_ssl_letsencrypt_getandinstall.groovy
@@ -3,14 +3,9 @@ job('madcore.ssl.letsencrypt.getandinstall') {
         stringParam('S3BucketName', '', '')
     }
     steps {
-        def command = '''#!/bin/bash
-     sudo /opt/madcore/bin/haproxy_get_ssl.py
-     CODE=$?
-     if [[ $CODE -ne 0 ]]; then
-     echo 'MADCORE GET CERT FAILED WITH POSITIVE CODE ($CODE). FORCING JENKINS BASH EXIT AS -1 (TO SHOW THE FAIL)'
-     exit -1
-fi
-'''
+        def command = """#!/bin/bash
+        sudo /opt/madcore/bin/haproxy_get_ssl.py
+"""
         shell(command)
     }
     publishers {
