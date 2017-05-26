bc4d50f764c75492bff064f5b965877abb4497f7
Peter Styk <peter@styk.tv>
2017-05-26 10:59:08 +0100

    shebang experiments

--- ci-vagrant/ci-init-vagrant.sh
+++ ci-vagrant/ci-init-vagrant.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -xak
+#!/bin/bash -xakv +e
 # Madcore Core (c) 2015-2017 Madcore Ltd on MIT License
 # Madcore is a trademark of Madcore Ltd in United Kingdom
 # All other trademarks belong to their respective owners
